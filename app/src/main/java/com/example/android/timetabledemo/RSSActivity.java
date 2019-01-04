d40f43192e698c457c00a80a051715ca65f56d3a
kev233 <44610339+kev233@users.noreply.github.com>
2019-01-04 15:16:37 +0100

    MensaPlan + Toolbar

--- app/src/main/java/com/example/android/timetabledemo/RSSActivity.java
+++ app/src/main/java/com/example/android/timetabledemo/RSSActivity.java
@@ -1,45 +1,218 @@
 package com.example.android.timetabledemo;
 
-import android.app.ListActivity;
+import android.app.Dialog;
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.AsyncTask;
+import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.os.StrictMode;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.Window;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.support.v7.widget.Toolbar;
 
+import org.xmlpull.v1.XmlPullParser;
+import org.xmlpull.v1.XmlPullParserException;
+import org.xmlpull.v1.XmlPullParserFactory;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.util.ArrayList;
-import java.util.Timer;
-import java.util.TimerTask;
 
-public class RSSActivity extends ListActivity {
+public class RSSActivity extends AppCompatActivity {
 
+    ListView lvRss;
+    ArrayList<String> titles;
+    ArrayList<String> links;
+    ArrayList<String> date;
+    private Toolbar toolbar;
+
+    private void setupUIViews(){
+        toolbar = (Toolbar) findViewById(R.id.ToolbarRss);
+        lvRss = (ListView)findViewById(R.id.lvRss);
+    }
+
+    private void initToolbar(){
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setTitle("Mensa");
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_rss);
 
-        //Array von Titel zu Preis geändert als Test ob die Übergabe funktioniert -> es funktioniert
+        setupUIViews();
+        initToolbar();
 
-        ArrayList<String> headlines = new ArrayList<>();
-        ArrayList<String> preis = new ArrayList<>();
+        lvRss = (ListView) findViewById(R.id.lvRss);
 
-        RetrieveFeed getXML = new RetrieveFeed();
-        getXML.execute();
-        headlines = getXML.heads();
-        preis = getXML.preis();
+        titles = new ArrayList<String>();
+        links = new ArrayList<String>();
+        date = new ArrayList<String>();
 
-        //Künstliche Pause da sonst Feed unter Umständen nicht angezeigt wird
-        try {
-            Thread.sleep(1000);
-        } catch(InterruptedException e) {
-            // Process exception
+        lvRss.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+
+                Uri uri = Uri.parse(links.get(position));
+                Intent intent = new Intent(Intent.ACTION_VIEW, uri);
+                startActivity(intent);
+
+            }
+        });
+        new ProcessInBackground().execute();
+    }
+
+
+    public InputStream getInputStream(URL url)
+    {
+        try
+        {
+            //openConnection() returns instance that represents a connection to the remote object referred to by the URL
+            //getInputStream() returns a stream that reads from the open connection
+            return url.openConnection().getInputStream();
+        }
+        catch (IOException e)
+        {
+            return null;
+        }
+    }
+
+    public class ProcessInBackground extends AsyncTask<Integer, Void, Exception>
+    {
+        ProgressDialog progressDialog = new ProgressDialog(RSSActivity.this);
+
+        Exception exception = null;
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+
+            progressDialog.setMessage("Speiseplan wird abgerufen, einen Moment");
+            progressDialog.show();
         }
 
-        // Binding data
-        ArrayAdapter adapter = new ArrayAdapter(this,
-                android.R.layout.simple_list_item_1, preis);
+        @Override
+        protected Exception doInBackground(Integer... params) {
 
-        setListAdapter(adapter);
+            try
+            {
+                URL url = new URL("https://politepol.com/feed/44147");
 
+                //creates new instance of PullParserFactory that can be used to create XML pull parsers
+                XmlPullParserFactory factory = XmlPullParserFactory.newInstance();
+
+                //Specifies whether the parser produced by this factory will provide support
+                //for XML namespaces
+                factory.setNamespaceAware(false);
+
+                //creates a new instance of a XML pull parser using the currently configured
+                //factory features
+                XmlPullParser xpp = factory.newPullParser();
+
+                // We will get the XML from an input stream
+                xpp.setInput(getInputStream(url), "UTF_8");
+
+                /* We will parse the XML content looking for the "<title>" tag which appears inside the "<item>" tag.
+                 * We should take into consideration that the rss feed name is also enclosed in a "<title>" tag.
+                 * Every feed begins with these lines: "<channel><title>Feed_Name</title> etc."
+                 * We should skip the "<title>" tag which is a child of "<channel>" tag,
+                 * and take into consideration only the "<title>" tag which is a child of the "<item>" tag
+                 *
+                 * In order to achieve this, we will make use of a boolean variable called "insideItem".
+                 */
+                boolean insideItem = false;
+
+                // Returns the type of current event: START_TAG, END_TAG, START_DOCUMENT, END_DOCUMENT etc..
+                int eventType = xpp.getEventType(); //loop control variable
+
+                while (eventType != XmlPullParser.END_DOCUMENT)
+                {
+                    //if we are at a START_TAG (opening tag)
+                    if (eventType == XmlPullParser.START_TAG)
+                    {
+                        //if the tag is called "item"
+                        if (xpp.getName().equalsIgnoreCase("item"))
+                        {
+                            insideItem = true;
+                        }
+                        //if the tag is called "title"
+                        else if (xpp.getName().equalsIgnoreCase("title"))
+                        {
+                            if (insideItem)
+                            {
+                                // extract the text between <title> and </title>
+                                titles.add(xpp.nextText());
+                            }
+                        }
+                        //if the tag is called "link"
+                        else if (xpp.getName().equalsIgnoreCase("link"))
+                        {
+                            if (insideItem)
+                            {
+                                // extract the text between <link> and </link>
+                                links.add(xpp.nextText());
+                            }
+                        }
+                    }
+                    //if we are at an END_TAG and the END_TAG is called "item"
+                    else if (eventType == XmlPullParser.END_TAG && xpp.getName().equalsIgnoreCase("item"))
+                    {
+                        insideItem = false;
+                    }
+
+                    eventType = xpp.next(); //move to next element
+                }
+
+
+            }
+            catch (MalformedURLException e)
+            {
+                exception = e;
+            }
+            catch (XmlPullParserException e)
+            {
+                exception = e;
+            }
+            catch (IOException e)
+            {
+                exception = e;
+            }
+
+            return exception;
+
+        }
+
+
+
+        @Override
+        protected void onPostExecute(Exception s) {
+            super.onPostExecute(s);
+
+            ArrayAdapter<String> adapter = new ArrayAdapter<String>(RSSActivity.this, android.R.layout.simple_list_item_1, titles);
+
+            lvRss.setAdapter(adapter);
+
+
+            progressDialog.dismiss();
+        }
     }
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch(item.getItemId()){
+            case android.R.id.home : {
+                onBackPressed();
+            }
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
 }
